bcf5c2d0a63c8c42ccd52e03cc8c7885c6e0b0ea
Emuohwo <warriisaac@outlook.com>
2022-04-07 04:04:22 -1100

    get categories & category detail

--- routes/categories.js
+++ routes/categories.js
@@ -11,4 +11,38 @@ router.get(`/`, async (req, res) =>{
     res.send(categoryList);
 })
 
+router.post('/', async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    })
+    category = await category.save();
+
+    if (!category)
+      return res.status(404).send('the category cannot be created')
+
+    res.status(200).send(category);
+})
+
+router.delete('/:id', (req, res) => {
+    Category.findByIdAndRemove(req.params.id).then((category) => {
+        if (category) {
+            return res.status(200).json({ success: true, message: 'category was deleted successfully!'})
+        } else {
+            return res.status(404).json({ success: false, message: 'category not found'})
+        }
+    }).catch(err => {
+        return res.status(400).json({ success: false, error: err})
+    })
+})
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id)
+
+    if (!category) {
+        return res.status(404).json({ message: 'The category with the given ID was not found.'})
+    }
+    res.status(200).send(category)
+})
+
 module.exports =router;
