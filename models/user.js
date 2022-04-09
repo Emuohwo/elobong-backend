dce5d3af6413b6f922fd2028438e33ddacabf1f5
Emuohwo <warriisaac@outlook.com>
2022-04-09 01:02:29 -1100

    Change _id to id

--- models/user.js
+++ models/user.js
@@ -9,4 +9,13 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+})
+
+
 exports.User = mongoose.model('User', userSchema);
